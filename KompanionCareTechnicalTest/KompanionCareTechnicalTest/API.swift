f261d06f147f6cfa4e2fb2841d1b3e9fb7f3402f
Romain <romain.dubreucq@withings.com>
2025-04-27 16:09:47 +0200

    project(): add api key and composable architecture package

---
+++ KompanionCareTechnicalTest/KompanionCareTechnicalTest/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  KompanionCareTechnicalTest
+//
+//  Created by Romain Dubreucq on 27/04/2025.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let weatherAPIKey = Bundle.main.infoDictionary?["WEATHER_API_KEY"] as? String
+}
